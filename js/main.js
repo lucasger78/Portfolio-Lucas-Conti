c71588654604664699c427fd670ca60c3fe4add3
Lucas Conti <lucasger78@hotmail.com>
2023-02-16 01:17:00 -0300

    90%

--- js/main.js
+++ js/main.js
@@ -61,4 +61,20 @@ Menu
 
 })();
 
+ // Portfolio isotope and filter
+//  var portfolioIsotope = $('.portfolio-container').isotope({
+// 	itemSelector: '.portfolio-item',
+// 	layoutMode: 'fitRows'
+// });
+// $('#portfolio-flters li').on('click', function () {
+// 	$("#portfolio-flters li").removeClass('active');
+// 	$(this).addClass('active');
+
+// 	portfolioIsotope.isotope({filter: $(this).data('filter')});
+// });
+
+
+	
+
+
 
